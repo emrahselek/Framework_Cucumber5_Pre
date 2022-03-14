37ef8ac9ddd6a8e6db24fc97a0341127120ca442
~!@#$%^&*()_+}{ <88084445+emrahselek@users.noreply.github.com>
2022-03-13 23:56:52 -0400

    es

--- src/test/java/seleniumCourse/S16_WindowHandle.java
+++ src/test/java/seleniumCourse/S16_WindowHandle.java
@@ -1,4 +1,36 @@
 package seleniumCourse;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import utilities.Driver;
+
+import java.util.Objects;
+import java.util.Set;
+
 public class S16_WindowHandle {
+
+    @Test
+    public void test1() throws InterruptedException {
+
+       Driver.getDriver().get("https://www.w3schools.com/colors/colors_picker.asp");
+
+       WebElement facabookLink = Driver.getDriver().findElement(By.xpath("//a[@title='Facebook']"));
+       facabookLink.click();
+
+       Driver.getDriver().getWindowHandle();
+       Thread.sleep(3000);
+
+       Set<String> handles = Driver.getDriver().getWindowHandles();
+
+        Object[] array = handles.toArray();
+        String sonSayfaninHandleDegeri = array[array.length-1].toString();
+        System.out.println();
+
+        Driver.getDriver().switchTo().window(sonSayfaninHandleDegeri);
+
+
+
+
+    }
 }
